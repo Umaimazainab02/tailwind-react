228e451f8b2f0a02ad84700b8573ce09147bfc31
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-05 12:28:40 +0500

    Added loop practice questions

--- src/components/Section1/BottomRight.jsx
+++ src/components/Section1/BottomRight.jsx
@@ -3,8 +3,8 @@ import Rightcard from './Rightcard'
 const BottomRight = (props) => {
     return (
         <div className=" w-2/3 h-full overflow-x-auto flex flex-row flex-nowrap gap-4 px-8">
-            {props.cardData.map(function (elem) {
-                return <Rightcard image={elem.image} id={elem.id} text={elem.text} status={elem.status}  />
+            {props.cardData.map(function (elem ,idx) {
+                return <Rightcard key={idx} image={elem.image} id={elem.id} color={elem.color} text={elem.text} status={elem.status}  />
             })}
         </div>
     )
--- src/components/Section1/Rightcard.jsx
+++ src/components/Section1/Rightcard.jsx
@@ -4,18 +4,20 @@ import { RiArrowRightLine } from "@remixicon/react";
 const Rightcard = (props) => {
     return (
         <div className=" w-65 h-full  shrink-0 relative overflow-hidden rounded-3xl">
-            <img className="w-full h-full object-cover brightness-60" src={props.image} alt="" />
+            <img className="w-full h-full object-cover  brightness-60" src={props.image} alt="" />
 
             <div className="absolute bottom-0 left-0 right-0 h-full  p-6 flex justify-between flex-col">
-                <div className=" font-bold text-black bg-white h-10 w-10 rounded-full flex items-center justify-center text-2xl">
+                <div className=" font-bold text-black bg-white hover:bg-purple-400 h-10 w-10 rounded-full flex items-center justify-center text-2xl">
                     {props.id}
                 </div>
                 <div>
                     <h3 className='text-gray-300 text-lg px-3 mb-7'>{props.text}</h3>
                     <div className="flex flex-row  gap-2 mt-4 text-white font-bold">
-                        <button className="bg-blue-500 text-white py-2 px-4 rounded-full">{props.status}</button>
-                        <RiArrowRightLine className="text-white bg-blue-500 rounded-full h-10 w-10" />
-
+                        <button style={{ backgroundColor: props.color }} className=" text-white py-2 px-4 rounded-full   hover:brightness-75">{props.status}</button>
+                        <RiArrowRightLine
+                            style={{ backgroundColor: props.color }}
+                            className="text-white rounded-full h-10 w-10"
+                        />
                     </div>
                 </div>
             </div>
